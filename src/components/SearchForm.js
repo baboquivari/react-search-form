c85638e399b8ac4a6994eb8b82d6e50998944b1c
Tony Sian <tony.sian@skybettingandgaming.com>
2019-03-09 17:21:09 +0000

    basic CSS started

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import CategorySearchBar from './category/CategorySearchBar';
 import LocationSearchBar from './location/LocationSearchBar';
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+import background from '../images/xing-bg-984x400.png';
+import sea from '../images/sea.jpg';
 
 const GlobalStyle = createGlobalStyle`
   /* Removes default rendered arrow on <datalist> elements */
   input::-webkit-calendar-picker-indicator {
 	  display: none;
   }
+
+  html, body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-image: url(${sea});
+  }
+`
+
+const Container = styled.div`
+    background-image: url(${background});
+    width: 984px;
+    height: 400px;
 `
 
 class SearchForm extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <GlobalStyle />
         <CategorySearchBar/>
         <LocationSearchBar/>
-      </div>
+      </Container>
     );
   }
 }
